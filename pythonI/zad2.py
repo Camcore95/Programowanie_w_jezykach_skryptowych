e269f7cdfd6d19f713b0864148b358c444592049
Camcore95 <38618559+Camcore95@users.noreply.github.com>
2018-12-01 15:59:35 +0000

    Update zad2.py

--- pythonI/zad2.py
+++ pythonI/zad2.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3
-
-imie, nazwisko, rok = input("Podaj imie, nazwisko i rok urodzenia ").split(" ")
+name, surname, year = input("Enter name, surname and birth year: ").split(" ")
+print("Your name is: ", name)
+print("Your surname is: ", surname)
+print("Year of your birth is: ", year)
